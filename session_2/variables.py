492ee64043d9e15d09253dad0467954e4fb32e2c
profjuarezbarbosajr <juarez.barbosa@ibat.ie>
2023-02-24 10:18:36 +0000

    @profjuarezbarbosajr

---
+++ session_2/variables.py
@@ -0,0 +1,15 @@
+# this is a comment
+
+my_string = "Juarez"      #my_string is of type string
+print (my_string)
+my_string = "Junior"
+print (my_string)
+
+first_number = 100      #first_number is of type int
+print (first_number)
+
+second_number = 200.0 #second_number is of type float
+print (second_number)
+
+boolean_value = True
+print(boolean_value)
---
+++ session_2/variables_concatenation.py
@@ -0,0 +1,14 @@
+# this is a comment
+
+my_string = "Juarez"      #my_string is of type string
+print ("My name is: " + my_string)
+
+first_number = 100      #first_number is of type int
+first_number = 100 * 2
+print ("First number is: " + str(first_number))
+
+second_number = 200.0 #second_number is of type float
+print ("Second number is: " + str(second_number))
+
+boolean_value = True
+print("The boolean value is: " + str(boolean_value))
